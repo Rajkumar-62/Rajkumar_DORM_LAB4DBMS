6ddc47b1f87af3d09523fa898a2c1f8ebc949f31
Rajkumar-62 <113999215+Rajkumar-62@users.noreply.github.com>
2022-11-28 20:16:04 +0530

    Add files via upload

---
+++ shah.sql
@@ -0,0 +1,139 @@
+show databases
+create database e_commerce
+show databases
+use e_commerce
+create table supplier
+(SUPP_ID INT Primary Key,
+SUPP_NAME varchar(50) NOT NULL,
+SUPP_CITY varchar(50) NOT NULL,
+SUPP_PHONE varchar(50) NOT NULL)
+desc supplier
+show tables
+create table customer
+(CUS_ID INT Primary Key,
+CUS_NAME VARCHAR(20) NOT NULL,
+CUS_PHONE VARCHAR(10) NOT NULL,
+CUS_CITY VARCHAR(30) NOT NULL,
+CUS_GENDER CHAR)
+desc customer
+
+create table category
+(CAT_ID INT Primary Key,
+CAT_NAME VARCHAR(20) NOT NULL)
+desc category
+
+create table product
+(PRO_ID INT Primary Key,
+PRO_NAME VARCHAR(20) DEFAULT 'Dummy',
+PRO_DESC VARCHAR(60),
+CAT_ID INT Foreign Key)
+desc product
+
+create table supplier_pricing
+(PRICING_ID INT Primary Key,
+PRO_ID INT Foreign Key,
+SUPP_ID INT Foreign Key,
+SUPP_PRICE INT DEFAULT 0)
+desc supplier_pricing
+
+create table order
+(ORD_ID INT Primary Key,
+ORD_AMOUNT INT NOT NULL,
+ORD_DATE DATE NOT NULL,
+CUS_ID INT Foreign Key,
+PRICING_ID INT Foreign Key)
+desc order
+
+create table rating
+(RAT_ID INT Primary Key,
+ORD_ID INT Foreign Key,
+RAT_RATSTARS INT NOT NULL)
+desc rating
+
+insert into supplier (SUPP_ID,SUPP_NAME,SUPP_CITY,SUPP_PHONE)
+values (1,"Rajesh Retails","Delhi",1234567890),
+	   (2,"Appario Ltd.","Mumbai",2589631470),
+	   (3,"Knome products","Banglore",9785462315),
+       (4,"Bansal Retails","Kochi",8975463285),
+       (5,"Mittal Ltd.","Lucknow",7898456532)
+       
+insert into customer (CUS_ID,CUS_NAME,CUS_PHONE,CUS_CITY,CUS_GENDER)
+values (1,"AAKASH",9999999999,"DELHI",M),
+       (2,"AMAN",9785463215,"NOIDA",M),
+       (3,"NEHA",9999999999,"MUMBAI",F),
+	   (4,"MEGHA",9994562399,"KOLKATA",F),
+       (5,"PULKIT",7895999999,"LUCKNOW",M)
+
+insert into Category (CAT_ID,CAT_NAME)
+values (1,"BOOKS"),
+       (2,"GAMES"),
+       (3,"GROCERIES),
+       (4,"ELECTRONICS),
+       (5,"CLOTHES")
+
+insert into Product (PRO_ID,PRO_NAME,PRO_DESC,CAT_ID)
+values (1,"GTA V","Windows 7 and above with i5 processor and 8GB RAM",2),
+       (2,"TSHIRT","SIZE-L with Black, Blue and White variations",5),
+       (3,"ROG LAPTOP","Windows 10 with 15inch screen, i7 processor, 1TB SSD",4),
+       (4,"OATS","Highly Nutritious from Nestle",3),
+	   (5,"HARRY POTTER","Best Collection of all time by J.K Rowling",1),
+       (6,"MILK","1L Toned MIlk",3),
+       (7,"Boat Earphones","1.5Meter long Dolby Atmos",4),
+       (8,"Jeans","Stretchable Denim Jeans with various sizes and color",5),
+       (9,"Project IGI","compatible with windows 7 and above",2),
+       (10,"Hoodie","Black GUCCI for 13 yrs and above",5),
+	   (11,"Rich Dad Poor Dad","Written by RObert Kiyosaki",1),
+       (12,"Train Your Brain","By Shireen Stephen",1)
+
+insert into Supplier_pricing (PRICING_ID,PRO_ID,SUPP_ID,SUPP_PRICE)
+values (1,1,2,1500),
+       (2,3,5,30000),
+	   (3,5,1,3000),
+       (4,2,3,2500),
+       (5,4,1,1000)
+
+insert into Order (ORD_ID,ORD_AMOUNT,ORD_DATE,CUS_ID,PRICING_ID)
+values (101,1500,2021-10-06,2,1),
+       (102,1000,2021-10-12,3,5),
+       (103,30000,2021-09-16,5,2),
+       (104,1500,2021-10-05,1,1),
+       (105,3000,2021-08-16,4,3),
+       (106,1450,2021-08-18,1,9),
+       (107,789,2021-09-01,3,7),
+       (108,780,2021-09-07,5,6),
+       (109,3000,2021-00-10,5,3),
+       (110,2500,2021-09-10,2,4),
+       (111,1000,2021-09-15,4,5),
+       (112,789,2021-09-16,4,7),
+       (113,31000,2021-09-16,1,8),
+       (114,1000,2021-09-16,3,5),
+       (115,3000,2021-09-16,5,3),
+       (116,99,2021-09-17,2,14)
+
+insert into Rating (RAT_ID,ORD_ID,RAT_RATSTARS)
+values (1,101,4),
+       (2,102,3),
+       (3,103,1),
+       (4,104,2),
+       (5,105,4),
+       (6,106,3),
+       (7,107,4),
+       (8,108,4),
+       (9,109,3),
+       (10,110,5),
+       (11,111,3),
+       (12,112,4),
+       (13,113,2),
+       (14,114,1),
+       (15,115,1),
+       (16,116,0)
+
+SELECT SUM (ORD_AMOUNT)
+FROM order
+WHERE ORD_AMOUNT = 3000 AND > 3000
+
+SELECT order.ORD_ID, customer.CUS_ID, customer.CUS_NAME
+FROM order
+INNER JOIN customer ON order.CUS_ID=customer.CUS_NAME
+WHERE CUS_ID=2
+
